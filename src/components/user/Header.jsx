e325ae62999535f64c8287482c7ccc50fced413d
rootrsk <rootrsk@gmail.com>
2025-01-26 03:42:57 +0530

    updating header link

--- src/components/user/Header.jsx
+++ src/components/user/Header.jsx
@@ -10,17 +10,20 @@ function Header() {
             <ApplicationSidebar open={false} />
             <div className="logo-wrapper">
                 <img src={logo} alt="App Logo" />
-                <h1 className="app-name">SmartHome</h1>
+                {/* <h1 className="app-name">SmartHome</h1> */}
             </div>
             <nav className="right-child">
                 <div className="links">
-                    <Link to="/" className="nav-link">Home</Link>
-                    <Link to="/app/dashboard" className="nav-link">Dashboard</Link>
+                    {/* <Link to="/" className="nav-link">Home</Link>
+                    <Link to="/app/dashboard" className="nav-link">Dashboard</Link> */}
                     <Link to="/app/switches" className="nav-link">Switches</Link>
+                    <Link to="/app/remote" className="nav-link">Remote</Link>
+                    <Link to="/app/search" className="nav-link">Search</Link>
+
                 </div>
-                <div className="profile">
+                {/* <div className="profile">
                     <img src="" alt="User Profile" />
-                </div>
+                </div> */}
             </nav>
         </header>
     );
